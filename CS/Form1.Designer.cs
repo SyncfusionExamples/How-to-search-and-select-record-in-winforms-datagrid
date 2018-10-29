f5b8151c3b22392d5d1e432a672ae7ca7ac8bebb
mohanram.anbukkarasu <mohanram.anbukkarasu@syncfusion.com>
2018-10-29 16:53:36 +0530

    Samples has been committed.

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using Syncfusion.WinForms.DataGrid;
+using Syncfusion.WinForms.DataGrid.Enums;
+using System.Windows.Forms;
+
+namespace GettingStarted
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfDataGrid = new Syncfusion.WinForms.DataGrid.SfDataGrid();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sfDataGrid
+            // 
+            this.sfDataGrid.AccessibleName = "Table";
+            this.sfDataGrid.AllowDraggingColumns = true;
+            this.sfDataGrid.AllowFiltering = true;
+            this.sfDataGrid.AllowResizingColumns = true;
+            this.sfDataGrid.AllowResizingHiddenColumns = true;
+            this.sfDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sfDataGrid.AutoSizeColumnsMode = Syncfusion.WinForms.DataGrid.Enums.AutoSizeColumnsMode.Fill;
+            this.sfDataGrid.BackColor = System.Drawing.SystemColors.Window;
+            this.sfDataGrid.Location = new System.Drawing.Point(10, 5);
+            this.sfDataGrid.Name = "sfDataGrid";
+            this.sfDataGrid.ShowGroupDropArea = false;
+            this.sfDataGrid.Size = new System.Drawing.Size(782, 580);
+            this.sfDataGrid.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(798, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Here";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1006, 593);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.sfDataGrid);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Getting Started";
+            ((System.ComponentModel.ISupportInitialize)(this.sfDataGrid)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region API Definition
+
+        private SfDataGrid sfDataGrid;
+        
+        #endregion
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Syncfusion.WinForms.Controls;
+using Syncfusion.WinForms.DataGrid.Styles;
+using System.Windows.Forms;
+using Syncfusion.WinForms.DataGrid.Enums;
+using Syncfusion.WinForms.DataGrid;
+
+namespace GettingStarted
+{   
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            var data = new OrderInfoCollection();
+            sfDataGrid.DataSource = data.OrdersListDetails;
+        }
+
+        #endregion 
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            this.sfDataGrid.SearchController.Search(this.textBox1.Text);
+        }
+
+        private void nextButton_Click(object sender, System.EventArgs e)
+        {
+            this.sfDataGrid.SearchController.FindNext(this.textBox1.Text);
+            SetSelectedItem();
+        }
+
+        private void previousButton_Click(object sender, System.EventArgs e)
+        {
+            this.sfDataGrid.SearchController.FindPrevious(this.textBox1.Text);
+            SetSelectedItem();
+        }
+
+        private void SetSelectedItem()
+        {
+            var rowIndex = this.sfDataGrid.SearchController.CurrentRowColumnIndex.RowIndex;
+            var recordIndex = this.sfDataGrid.TableControl.ResolveToRecordIndex(rowIndex);
+            this.sfDataGrid.SelectedIndex = recordIndex;
+        }
+    }
+}
